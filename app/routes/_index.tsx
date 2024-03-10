b0aaf87e168975631d67279ec236d2b394394742
Rishi Viswanathan <rishi.aangarai@gmail.com>
2024-03-10 09:00:01 +0530

    feat: bootstrap app

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (  
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold">Codebin</h1>
+    </div>
+  );
+}
+
+export default Home
