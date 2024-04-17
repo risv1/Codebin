bfb6afd812ea6b37792edfd647bc12e97771eeb0
risv1 <rishi.aangarai@gmail.com>
2024-04-18 02:25:35 +0530

    feat: add drizzle

---
+++ database/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, varchar, date } from "drizzle-orm/pg-core"
+
+export const users = pgTable("users", {
+    id: varchar("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").unique().notNull(),
+    password: text("password").notNull(),
+    created_at: date("created_at").defaultNow(),
+    updated_at: date("updated_at").defaultNow(),
+  });
---
+++ drizzle/0000_real_caretaker.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" varchar PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"created_at" date DEFAULT now(),
+	"updated_at" date DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
