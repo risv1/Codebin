a903fa5b83b5c7fdb7fc85503271915bdb7ca4e0
risv1 <rishi.aangarai@gmail.com>
2024-04-24 08:58:22 +0530

    feat: add cert conn

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,14 @@ import { config } from "dotenv";
 
 config();
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 export default {
   schema: "./database/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.DB_URL!,
+    ssl: true
   },
 } satisfies Config;
